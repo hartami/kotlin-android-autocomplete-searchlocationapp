b12d6b452a6c5fe01c053f191dcbc5d732fb29eb
Aprilia-Hartami <apriliahartami@gmail.com>
2019-02-05 12:19:06 +0200

    update on data and parseXML

--- app/src/main/java/com/iteritory/searchlocationapp/DatabaseOpenHelper.kt
+++ app/src/main/java/com/iteritory/searchlocationapp/DatabaseOpenHelper.kt
@@ -6,7 +6,7 @@ import com.readystatesoftware.sqliteasset.SQLiteAssetHelper
 
 class DatabaseOpenHelper(context: Context) : SQLiteAssetHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
     companion object {
-        private val DATABASE_NAME = "MyExternalDatabase.db"
+        private val DATABASE_NAME = "DataSQLite.db"
         private val DATABASE_VERSION = 1
     }
 }
--- app/src/main/java/com/iteritory/searchlocationapp/MainActivity.kt
+++ app/src/main/java/com/iteritory/searchlocationapp/MainActivity.kt
@@ -1,32 +1,32 @@
 package com.iteritory.searchlocationapp
 
+import android.app.DownloadManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils.replace
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
-import org.json.JSONObject;
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
-import org.xmlpull.v1.XmlPullParserFactory;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-
-
-//import sun.text.normalizer.UTF16.append
+import okhttp3.*
+import org.json.JSONObject
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserException
+import org.xmlpull.v1.XmlPullParserFactory
+import java.io.IOException
+import java.io.InputStream
+import java.util.ArrayList
 
 
 
 class MainActivity : AppCompatActivity() {
 
-    val mydata = arrayListOf(
-        "Meisenweg 20789 Mannheim",
-        "Mullerstrasse 18789 Ausburg",
-        "Michaelestrasse 29332 Stuttgart",
-        "Hauptsrasse 12345 Heildelberg",
-        "Darwinstrasse 33323 Ulm",
-        "Panamastrasse 23412 Berlin",
-        "Paristrasse 13453 Leipzig"
+    val initialdata = arrayListOf(
+        "Meisenweg 45, 20789 Mannheim",
+        "Mullerstrasse 1, 18789 Ausburg",
+        "Michaelestrasse 89, 29332 Stuttgart",
+        "Hauptsrasse 12, 12345 Heildelberg",
+        "Darwinstrasse, 33323 Ulm",
+        "Panamastrasse, 23412 Berlin",
+        "Parisertrasse, 13453 Leipzig"
     )
 
 
@@ -34,18 +34,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // to obtain data from dataloc.xml
+        fetchJson()
+
+        // to call methods and obtain data from dataloc.xml
         parseXML()
 
-        // to obtain data from datalokal.json
+        // to call methods and obtain data from datalokal.json
         var jsonString: String? = null
         try {
             val inputStream: InputStream = assets.open("datalokal.json")
             jsonString = inputStream.bufferedReader().use { it.readText() }
+
             val foos = Response(jsonString)
             val dataJson= foos.data
-
-            mydata.add(dataJson.toString())
+            val length = foos.length()
+            for (i in 0..length-1) {
+                var eachdata = foos.data?.get(i).toString()
+                val cleaner = Regex("[^A-Za-z0-9]")
+                eachdata = cleaner.replace(eachdata, "")
+                var cleandata = eachdata.replace("street", "", true)
+                var cleandata2 = cleandata.replace("postalcode", ", ", true)
+                var cleandata3 = cleandata2.replace("city", " ", true)
+                initialdata.add(cleandata3)
+            }
 
         } catch (e: IOException) {
         }
@@ -61,19 +72,20 @@ class MainActivity : AppCompatActivity() {
 
 
 
-        // to perform autocomplete search
+        // to perform autocomplete when users input data
+        // using ArrayAdapter
         var adapter = ArrayAdapter<String>(
             this,
             android.R.layout.simple_spinner_dropdown_item,
-            mydata
+            initialdata
         )
         auto_complete_text_view_arrival.threshold = 1
         auto_complete_text_view_arrival.setAdapter(adapter)
         auto_complete_text_view.threshold = 1
         auto_complete_text_view.setAdapter(adapter)
-
     }
 
+    // to read XML file: dataloc.xml
     private fun parseXML() {
         val parserFactory: XmlPullParserFactory
         try {
@@ -90,8 +102,6 @@ class MainActivity : AppCompatActivity() {
         } catch (e: IOException) {
         }
     }
-
-
     @Throws(IOException::class, XmlPullParserException::class)
     private fun processParsing(parser: XmlPullParser) {
         val locations = ArrayList<Location>()
@@ -126,56 +136,51 @@ class MainActivity : AppCompatActivity() {
         getLocations(locations)
     }
 
+    // to process each location data from dataloc.xml
     private fun getLocations(locations: ArrayList<Location>) {
-        val builder = StringBuilder()
-
-
         for (location in locations) {
-            builder.append(location.streetname).append(" ").append(location.postal).append(" ").append(location.city)
+            val builder = StringBuilder()
+            builder.append(location.streetname).append(", ").append(location.postal).append(" ").append(location.city)
                 .append("\n")
+            initialdata.add(builder.toString())
         }
-        mydata.add(builder.toString())
-
     }
 
-
+    // to process each data from datalokal.json
     class Response(json: String) : JSONObject(json) {
         val type: String? = this.optString("type")
         val data = this.optJSONArray("data")
             ?.let { 0.until(it.length()).map { i -> it.optJSONObject(i) } } // returns an array of JSONObject
-            ?.map {Foo(it.toString())} // transforms each JSONObject of the array into Foo it.toString()
-
+            ?.map {Foo(it.toString())} // transforms each JSONObject of the array into Foo
     }
     class Foo(json: String) : JSONObject(json) {
-        val streetname: String? = this.optString("street")
-        val code = this.optInt("postal_code")
-        val cityname: String? = this.optString("city")
-        val fullname= streetname + code + cityname
+        var streetname: String? = this.optString("street")
+        var code = this.optInt("postal_code")
+        var cityname: String? = this.optString("city")
+        var fullname= streetname + code + cityname
     }
 
+    fun fetchJson(){
+        println("Success")
+        val url = "https://raw.githubusercontent.com/openaddresses/openaddresses/master/sources/de/berlin.json"
+        val client = OkHttpClient()
+        val request = Request.Builder().url(url).build()
+        client.newCall(request).enqueue(object: Callback{
+            override fun onResponse(call: Call, response: okhttp3.Response) {
+                val body = response.body()?.string()
+                println(body)
+            }
 
+            override fun onFailure(call: Call, e: IOException) {
+                println("Failed to execute request")
+            }
+
+        })
+
+
+    }
 }
 
 
-
-
-/*fun loadAddress(view: View) {
-    val res = getResources()
-    val `is` = res.openRawResource(R.raw.dataaddress)
-    val scanner = Scanner(`is`)
-
-    val builder = StringBuilder()
-
-    while (scanner.hasNextLine()) {
-        builder.append(scanner.nextLine())
-    }
-
-    parseJson(builder.toString())
-}
-
-private fun parseJson(a: String) {
-    val textDisplay = findViewById<View>(R.id.text_display) as TextView
-    textDisplay.setText(a)
-}  */
 
 
